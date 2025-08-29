1f9b8200ce110915c8134c8f50b487e4ee6b9229
java73 <dakshsingh814@gmail.com>
2025-08-29 11:25:20 +0530

    java73

---
+++ src/UserDefineException.java
@@ -0,0 +1,8 @@
+public class UserDefineException {
+    public void validate(int age) throws InvalidAgeException {
+        if(age<18)
+            throw new InvalidAgeException("Cannot caste vote, Age less than 18.");
+        else
+            System.out.println("Eligible to vote");
+    }
+}
